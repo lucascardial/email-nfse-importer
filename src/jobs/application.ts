0637c26c731d00744ea7cbd87268b3d63da6cd1e
Lucas Cardial <luccascardial@gmail.com>
2023-04-08 15:54:00 -0300

    refactor: jobs

--- src/jobs/application.ts
+++ src/jobs/application.ts
@@ -1,9 +1,9 @@
 import schedule from 'node-schedule';
-import { ExportToXmlJob } from './dailly_invoice_xml_export';
-import { FetchEmailsJob } from './fetch_email_attachments';
+import * as dotenv from 'dotenv'; 
 
-// schedule fetch email job to every 1 minute
-schedule.scheduleJob('*/1 * * * *', FetchEmailsJob);
+import { ExportToXmlJob } from './dailly_invoice_xml_export';
+
+dotenv.config()
 
 // schedule export to xml job to every day at 00:00
 schedule.scheduleJob('0 0 * * *', ExportToXmlJob);
