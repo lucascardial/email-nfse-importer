a78c460cc63275dc047bb10beb52d275dc691e29
Lucas Cardial <luccascardial@gmail.com>
2023-04-08 17:46:14 -0300

    refactor: def types

--- src/infrastructure/file-reader/xml/xml-file-reader.ts
+++ src/infrastructure/file-reader/xml/xml-file-reader.ts
@@ -8,7 +8,7 @@ export class XmlFileReader implements IXmlFileReader {
     async read<T = object>(path: string): Promise<T> {
         return new Promise((resolve, reject) => {
             const xml = readFileSync(path, 'utf-8');
-            parseString(xml, { explicitArray: false } ,(err, result) => {
+            parseString(xml, { explicitArray: false } ,(err: any, result: any) => {
                 if (err) {
                     reject(err);
                 } else {                    
