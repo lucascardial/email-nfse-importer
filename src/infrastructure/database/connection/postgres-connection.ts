429abe1eab1371b4dd72391b69fc2cb96772479c
Lucas Cardial <luccascardial@gmail.com>
2023-04-10 00:50:55 -0300

    fix: ssl db connection

--- src/infrastructure/database/connection/postgres-connection.ts
+++ src/infrastructure/database/connection/postgres-connection.ts
@@ -13,7 +13,8 @@ export class PostgresConnection implements IDBConnection {
             port: parseInt(process.env.DB_PORT!),
             database: process.env.DB_NAME,
             user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD
+            password: process.env.DB_PASSWORD,
+            ssl: { rejectUnauthorized: false } 
         });
         
     }
