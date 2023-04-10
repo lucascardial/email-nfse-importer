14e76641fd2602db028c22eae18b838df4f5508e
Lucas Cardial <luccascardial@gmail.com>
2023-04-10 10:15:12 -0300

    refact: fetch email

--- src/shells/fetch_email.ts
+++ src/shells/fetch_email.ts
@@ -13,24 +13,29 @@ dotenv.config()
 
 const handler = Container.resolve(ImportInvoiceFromXmlCommandHandler);
 
-async function run() {
-  console.log({
+let imapConnected = false;
+
+const config = {
+  imap: {
+    user: process.env.IMAP_USER,
+    password: process.env.IMAP_PASSWORD,
     host: process.env.IMAP_HOST,
     port: process.env.IMAP_PORT,
-  });
-  
-  const config = {
-    imap: {
-      user: process.env.IMAP_USER,
-      password: process.env.IMAP_PASSWORD,
-      host: process.env.IMAP_HOST,
-      port: process.env.IMAP_PORT,
-      tls: true,
-    },
-  };
-  
-  const imap = new MyImap(config);
-  await imap.connect().then(() =>console.log('connected'));
+    tls: true,
+  },
+};
+
+const imap = new MyImap(config);
+
+async function run() {  
+  if(!imapConnected) {
+    console.log(`connecting to ${process.env.IMAP_HOST}:${process.env.IMAP_PORT}`);
+    await imap.connect();
+    imapConnected = true;
+    console.log(`connected to ${process.env.IMAP_HOST}:${process.env.IMAP_PORT}`);
+
+  }
+
   await imap.openBox();
 
   const criteria = [];
@@ -39,36 +44,34 @@ async function run() {
 
   criteria.push(["SINCE", moment().format("MMMM DD, YYYY")]);
 
-  const emails = await imap.fetchEmails(criteria);
+  while(true) {
+    console.log('fetching emails ...');
+    
+    const emails = await imap.fetchEmails(criteria);
 
-  for (const email of emails) {
-    for (const file of email.files) {
-      if (file.mimetype !== "text/xml") continue;
-      const buffer = Buffer.from(file.buffer);
-
-      try {
-        const filename = `downloads/${v4()}.xml`;
-        writeFileSync(filename, buffer);
-        const command = new ImportInvoiceFromXmlCommand(filename);
-        await handler.execute(command);
-      } catch (error) {
-        console.error(error);
+    for (const email of emails) {
+      for (const file of email.files) {
+        if (file.mimetype !== "text/xml") continue;
+        const buffer = Buffer.from(file.buffer);
+  
+        try {
+          const filename = `downloads/${v4()}.xml`;
+          writeFileSync(filename, buffer);
+          const command = new ImportInvoiceFromXmlCommand(filename);
+          await handler.execute(command);
+        } catch (error) {
+          console.error(error);
+        }
       }
     }
-  }
 
-  await imap.end();
-}
-
-export async function FetchEmailsJob() {
-  while (true) {
-    try {
-      await run();
-      await new Promise((resolve) => setTimeout(resolve, 1000 * 5));
-    } catch (error) {
-      console.error(error);
-    }
+    await new Promise((resolve) => setTimeout(resolve, 1000 * 60));
   }
 }
 
-FetchEmailsJob()
+try {
+  run();
+} catch (error) {
+  imap.end()
+  throw error
+}
