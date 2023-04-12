a9b2524eda644dd33a5d86f733eea284c6de7d10
Lucas Cardial <luccascardial@gmail.com>
2023-04-12 14:29:38 -0300

    feat: report file

---
+++ src/domain/entities/report-file.ts
@@ -0,0 +1,17 @@
+import { v4 } from "uuid";
+
+export class ReportFile {
+    public readonly uid?: string;
+    public readonly fileName: string;
+    public readonly filePath: string;
+    public readonly fileType: string;
+    public readonly createdAt: Date;
+
+    constructor(props: ReportFile) {
+        this.uid = props.uid || v4();
+        this.fileName = props.fileName;
+        this.filePath= props.fileName;
+        this.fileType = props.fileType;
+        this.createdAt = props.createdAt;
+    }
+}
