8f84dadf1ceb36845dfdaef58fc5effafa1f34f2
Lucas Cardial <luccascardial@gmail.com>
2023-04-12 15:20:17 -0300

    refactor: delete script

--- src/shells/container.ts
+++
@@ -1,7 +0,0 @@
-import 'reflect-metadata'
-import { Container } from "inversify";
-import "../application/dependency-injection";
-import "../infrastructure/dependency-injection";
-
-export default new Container().addApplication().addInfrastructure()
-
