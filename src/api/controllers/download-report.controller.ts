bc6cf520531e81ec63c8b50c7c139b9035e9efad
Lucas Cardial <luccascardial@gmail.com>
2023-04-12 14:30:39 -0300

    feat(api): download report controller

---
+++ src/api/controllers/download-report.controller.ts
@@ -0,0 +1,34 @@
+import { inject, injectable } from "inversify";
+import { IReportFileRepository } from "../../application/persistence/report-file-repository.interface";
+import { Request, Response } from "express";
+import { rootDir } from "../helpers";
+
+@injectable()
+export class DownloadReportController {
+    constructor(
+        @inject("IReportFileRepository") private readonly reportRepository: IReportFileRepository
+    ){}
+
+    async downloadFile(request: Request, response: Response): Promise<any> {
+
+        const { file_uid } = request.query;
+
+        if(!file_uid) {
+            return response.status(400).json({
+                message: "Informe o uid do arquivo"
+            });
+        }
+        
+        const reportFile = await this.reportRepository.getByUid(file_uid as string);
+
+        if(!reportFile) {
+            return response.status(404).json({
+                message: "Arquivo não encontrado"
+            });
+        }
+
+        console.log(rootDir + '/' + reportFile.filePath);
+        
+        return response.download(rootDir + '/' + reportFile.filePath, reportFile.fileName);
+    }
+}
--- src/api/routes.ts
+++ src/api/routes.ts
@@ -8,6 +8,7 @@ import { RegisterUserController } from "./controllers/authentication/register-us
 import { LoginController } from "./controllers/authentication/login.controller";
 import { GetIssuerCompaniesController } from "./controllers/get-issuer-companies.controller";
 import { GetReceiverCompaniesController } from "./controllers/get-receiver-companies.controller";
+import { DownloadReportController } from "./controllers/download-report.controller";
 
 const routes = Router();
 
@@ -17,6 +18,7 @@ routes.get('/invoices', (req, res) => container.resolve(GetInvoiceByDateControll
 routes.get('/invoices/issuer', (req, res) => container.resolve(GetInvoiceByIssuerController).handle(req, res))
 routes.get('/companies', (req, res) => container.resolve(GetIssuerCompaniesController).handle(req, res))
 routes.get('/company-receivers', (req, res) => container.resolve(GetReceiverCompaniesController).handle(req, res))
+routes.get('/download-report', (req, res) => container.resolve(DownloadReportController).downloadFile(req, res))
 
 routes.post('/register', (req, res) => container.resolve(RegisterUserController).handle(req, res))
 routes.post('/login', (req, res) => container.resolve(LoginController).handle(req, res));
